00ecd917d9ac03c1267f198f84cdb86c3233bc32
Alistair MacDonald <alistairuk@users.noreply.github.com>
2022-09-17 20:32:11 +0100

    Adding more examples

--- examples/circle.py
+++ examples/circle.py
@@ -1,13 +1,14 @@
-"""Trundlebot octagon example"""
+"""Trundlebot circle example"""
 
 # Import the trundle library
 # You need to do this before you call any trundle commands
 import trundle
 
 # Loop through these commands 8 times
-for x in range(8):
+for x in range(360):
     # Drive forward 50cm
-    trundle.forward(50)
+    trundle.forward(1)
     # Turn right 45 degrees
-    trundle.right(45)
+    trundle.right(1)
+
 
