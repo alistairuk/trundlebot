8df7dcbdf008a41ddf4bcf56bf3bdaf2b06e9c59
Alistair MacDonald <alistairuk@users.noreply.github.com>
2022-09-17 20:31:46 +0100

    Fix to use correct trundle library

--- examples/octagon.py
+++ examples/octagon.py
@@ -7,14 +7,6 @@ import trundle
 # Loop through these commands 8 times
 for x in range(8):
     # Drive forward 50cm
-    trundle008.forward(50)
+    trundle.forward(50)
     # Turn right 45 degrees
-    trundle008.right(45)
-
-# Wait for 5 seconds
-# This gives us time to press the reset button if we want to stop
-sleep(5)
-
-# Turn left a full circle (anticlockwise)
-# This is so we can untable the wire
-trundle008.left(360)
+    trundle.right(45)
